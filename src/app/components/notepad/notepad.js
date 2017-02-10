b1194c23ef7234d2c0916c6e096dfcf6fa26669b
Travis CI <ikandrius@gmail.com>
2017-02-10 15:29:18 +0200

    Rewrite notepad to add ability to have multiple tabs

--- src/app/components/notepad/notepad.js
+++ src/app/components/notepad/notepad.js
@@ -1,26 +1,109 @@
 /* global chrome */
 
-import { Component } from "@angular/core";
+import { Component, Inject } from "@angular/core";
+import { DOCUMENT } from "@angular/platform-browser";
 
 @Component({
     selector: "notepad",
     template: `
-        <textarea class="input main-block-notepad"
-            [value]="notepad"
+        <ul class="notepad-header">
+            <li class="notepad-header-item"
+                *ngFor="let tab of tabs; let i = index"
+                [class.active]="activeTabIndex === i">
+                <button class="btn" (click)="hideInputAndSelectTab(i)" *ngIf="!tab.settingTitle">{{ tab.title }}</button>
+                <button class="icon-cancel font-btn notepad-remove-tab-btn"
+                    (click)="removeTab(i)"
+                    *ngIf="tabs.length > 1 && !tab.settingTitle">
+                </button>
+                <input class="input notepad-title-input" type="text" #input
+                    (keyup.enter)="setTitle(input)"
+                    *ngIf="tab.settingTitle">
+            </li>
+            <li>
+                <button class="icon-plus btn notepad-create-new-btn" (click)="createNewTab()"></button>
+            </li>
+        </ul>
+        <textarea class="input notepad"
+            [(ngModel)]="activeTabContent"
             (input)="onInput($event)"
-            (keydown)="preventLossOfFocus($event)">
+            (keydown.tab)="preventLossOfFocus($event)">
         </textarea>
     `
 })
 export class Notepad {
-    constructor() {
-        this.notepad = "";
+    static get parameters() {
+        return [[new Inject(DOCUMENT)]];
+    }
+
+    constructor(document) {
+        this.document = document;
+        this.tabs = [{
+            title: "Tab",
+            content: ""
+        }];
+        this.activeTabIndex = 0;
     }
 
     ngOnInit() {
         chrome.storage.sync.get("notepad", storage => {
-            this.notepad = storage.notepad || localStorage.getItem("notepad") || "";
+            if (typeof storage.notepad === "string") {
+                this.saveTabContent(storage.notepad);
+            }
+            else if (storage.notepad && storage.notepad.length) {
+                this.tabs = storage.notepad;
+            }
+            this.hideInputAndSelectTab(this.activeTabIndex);
         });
+    }
+
+    hideTitleInput() {
+        this.tabs = this.tabs.map(tab => {
+            tab.settingTitle = false;
+            return tab;
+        });
+    }
+
+    hideInputAndSelectTab(index) {
+        this.hideTitleInput();
+        this.selectTab(index);
+    }
+
+    selectTab(index) {
+        this.activeTabIndex = index;
+        this.activeTabContent = this.tabs[index].content;
+    }
+
+    createNewTab() {
+        this.hideTitleInput();
+        this.tabs.push({
+            title: "Tab",
+            content: "",
+            settingTitle: true
+        });
+        this.selectTab(this.tabs.length - 1);
+        setTimeout(() => {
+            this.document.querySelector(".notepad-title-input").focus();
+        });
+    }
+
+    removeTab(index) {
+        if (index === this.activeTabIndex) {
+            this.selectTab(0);
+        }
+        else if (index < this.activeTabIndex) {
+            this.activeTabIndex -= 1;
+        }
+        this.tabs.splice(index, 1);
+        this.saveTabs();
+    }
+
+    setTitle(input) {
+        const title = input.value.trim();
+        const tab = this.tabs[this.tabs.length - 1];
+
+        tab.title = title ? title : "Tab";
+        tab.settingTitle = false;
+        this.saveTabs();
     }
 
     insertSpace(elem) {
@@ -34,19 +117,22 @@ export class Notepad {
         elem.selectionEnd = elem.selectionStart;
     }
 
-    saveContent(content) {
-        chrome.storage.sync.set({ notepad: content });
+    saveTabContent(content) {
+        this.tabs[this.activeTabIndex].content = content;
+        this.saveTabs();
+    }
+
+    saveTabs() {
+        chrome.storage.sync.set({ notepad: this.tabs });
     }
 
     onInput(event) {
-        this.saveContent(event.target.value);
+        this.saveTabContent(event.target.value);
     }
 
     preventLossOfFocus(event) {
-        if (event.which === 9) {
-            event.preventDefault();
-            this.insertSpace(event.target);
-            this.saveContent(event.target.value);
-        }
+        event.preventDefault();
+        this.insertSpace(event.target);
+        this.saveTabContent(event.target.value);
     }
 }
