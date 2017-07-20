845268aa80512701a5cef68686fd5395cf3a7e2e
Andrius Kvedaras <ikandrius@gmail.com>
2017-07-20 17:54:44 +0300

    Tweak most visited new page panel

--- src/app/components/most-visited/most-visited.ts
+++ src/app/components/most-visited/most-visited.ts
@@ -22,29 +22,6 @@ export class MostVisited {
     }
 
     ngOnInit() {
-        this.loadMostVisited();
-    }
-
-    ngOnChanges(changes) {
-        const setting = changes.setting.currentValue;
-
-        if (setting && setting.resetMostVisited) {
-            this.resetMostVisited();
-        }
-    }
-
-    getMostVisited() {
-        chrome.topSites.get(data => {
-            this.mostVisited = {
-                display: data.slice(0, 8),
-                backup: data.slice(8)
-            };
-            this.addImages(this.mostVisited.display);
-            this.checkBackup();
-        });
-    }
-
-    loadMostVisited() {
         const mostVisited = JSON.parse(localStorage.getItem("most visited"));
 
         if (mostVisited) {
@@ -59,6 +36,27 @@ export class MostVisited {
             return;
         }
         this.getMostVisited();
+    }
+
+    ngOnChanges(changes) {
+        const setting = changes.setting.currentValue;
+
+        if (setting && setting.resetMostVisited) {
+            this.resetMostVisited();
+        }
+    }
+
+    getMostVisited() {
+        chrome.topSites.get(data => {
+            this.mostVisited = {
+                display: data.slice(0, 8).map(page => {
+                    page.image = this.getImage(page.url);
+                    return page;
+                }),
+                backup: data.slice(8)
+            };
+            this.checkBackup();
+        });
     }
 
     resetMostVisited() {
@@ -85,16 +83,9 @@ export class MostVisited {
     }
 
     getImage(url) {
-        const a = document.createElement("a");
-        a.href = url;
+        url = new URL(url);
 
-        return this.domSanitizer.bypassSecurityTrustUrl(`chrome://favicon/${a.protocol}//${a.hostname}${a.pathname}`);
-    }
-
-    addImages(pages) {
-        pages.forEach(page => {
-            page.image = this.getImage(page.url);
-        });
+        return this.domSanitizer.bypassSecurityTrustUrl(`chrome://favicon/${url.origin}`);
     }
 
     showNewThumbWindow() {
@@ -105,26 +96,11 @@ export class MostVisited {
         this.newThumbWindowVisible = false;
     }
 
-    checkIfSafeUrl(url) {
-        const isHttp = url.includes("http://");
-        const isHttps = url.includes("https://");
-        const includesWWW = url.includes("www.");
-
-        if (!isHttp && !isHttps && !includesWWW) {
-            return `http://www.${url}`;
+    appendProtocol(url) {
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            return `https://${url}`;
         }
-        if (!isHttp && !isHttps && includesWWW) {
-            return `http://${url}`;
-        }
-        if (isHttp || isHttps) {
-            if (includesWWW) {
-                return url;
-            }
-
-            const index = url.indexOf("://") + 3;
-
-            return `${url.substr(0, index)}www.${url.substr(index)}`;
-        }
+        return url;
     }
 
     processImage(image) {
@@ -136,8 +112,8 @@ export class MostVisited {
 
                 image.onload = function() {
                     const canvas = document.createElement("canvas");
-                    const width = 118;
-                    const height = 80;
+                    const width = 122;
+                    const height = 84;
 
                     canvas.width = width;
                     canvas.height = height;
@@ -151,33 +127,23 @@ export class MostVisited {
         });
     }
 
-    addPage(fileInput) {
-        const title = this.newPage.title;
-        const url = this.checkIfSafeUrl(this.newPage.url);
+    async addPage(fileInput) {
+        if (!this.newPage.url) {
+            return;
+        }
+        const title = this.newPage.title || this.newPage.url;
+        const url = this.appendProtocol(this.newPage.url);
         const image = fileInput.files[0];
-
         fileInput.value = "";
         this.newPage = {};
-        if (image) {
-            this.processImage(image)
-            .then(processedImage => {
-                this.mostVisited.display.push({
-                    image: processedImage,
-                    uploaded: true,
-                    title,
-                    url
-                });
-                localStorage.setItem("most visited", JSON.stringify(this.mostVisited));
-            });
-        }
-        else {
-            this.mostVisited.display.push({
-                image: this.getImage(url),
-                title,
-                url
-            });
-            localStorage.setItem("most visited", JSON.stringify(this.mostVisited));
-        }
+
+        this.mostVisited.display.push({
+            image: image ? await this.processImage(image) : this.getImage(url),
+            uploaded: !!image,
+            title,
+            url
+        });
+        localStorage.setItem("most visited", JSON.stringify(this.mostVisited));
         this.checkBackup();
         this.hideNewThumbWindow();
     }
