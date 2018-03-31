a370482440d377979582fa4ec2c492fa79d391f5
Andrius Kvedaras <ikandrius@gmail.com>
2018-03-31 12:19:28 +0300

    Rewrite notepad component

--- src/app/components/notepad/notepad.ts
+++ src/app/components/notepad/notepad.ts
@@ -1,61 +1,77 @@
-import { Component, Inject, Input } from "@angular/core";
-import { DOCUMENT } from "@angular/platform-browser";
+import { Component, Input } from "@angular/core";
 import { ChromeStorageService } from "../../services/chromeStorageService";
 
 @Component({
     selector: "notepad",
     template: `
-        <div class="main-block-content" [class.visible]="isVisible">
-            <ul class="main-block-content-header">
-                <li *ngIf="tabs.length > 4">
-                    <button class="btn-icon notepad-shift-btn" (click)="prevVisibleTabs()" [disabled]="shift <= 0">
+        <div class="main-block-content notepad"
+            [class.visible]="isVisible"
+            [class.active]="initialized">
+            <div class="main-block-content-header">
+                <button class="btn-icon notepad-header-btn"
+                    *ngIf="tabs.length > 4"
+                    (click)="prevVisibleTabs()"
+                    [disabled]="shift <= 0">
+                    <svg viewBox="0 0 24 24">
+                        <use href="#chevron-left"></use>
+                    </svg>
+                </button>
+                <ul class="notepad-tabs">
+                    <li class="notepad-tab"
+                        *ngFor="let tab of tabs; let i = index"
+                        [class.active]="activeTabIndex === i"
+                        [class.hidden]="i < shift || i >= shift + 4">
+                        <button class="btn-icon notepad-tab-select-btn"
+                            (click)="selectTab(i)">{{ tab.title }}</button>
+                        <button class="btn-icon notepad-tab-remove-btn"
+                            (click)="setTabForRemoval(i)"
+                            *ngIf="tabs.length > 1"
+                            title="Remove tab">
+                            <svg viewBox="0 0 24 24">
+                                <use href="#cross"></use>
+                            </svg>
+                        </button>
+                    </li>
+                </ul>
+                <button class="btn-icon notepad-header-btn"
+                    *ngIf="tabs.length > 4"
+                    (click)="nextVisibleTabs()"
+                    [disabled]="shift + 4 >= tabs.length">
                         <svg viewBox="0 0 24 24">
-                            <use href="#chevron-left"></use>
+                            <use href="#chevron-right"></use>
                         </svg>
-                    </button>
-                </li>
-                <li class="notepad-header-item"
-                    *ngFor="let tab of visibleTabs; let i = index"
-                    [class.active]="activeTabIndex === i">
-                    <button class="btn-icon notepad-select-tab-btn"
-                        (click)="hideInputAndSelectTab(i)"
-                        *ngIf="!tab.settingTitle">{{ tab.title }}</button>
-                    <button class="btn-icon notepad-remove-tab-btn"
-                        (click)="setTabForRemoval(i)"
-                        *ngIf="tabs.length > 1 && !tab.settingTitle"
-                        title="Remove tab">
+                </button>
+                <button class="btn-icon notepad-header-btn"
+                    (click)="showTabCreateModal()" title="Create tab">
+                    <svg viewBox="0 0 24 24">
+                        <use href="#plus"></use>
+                    </svg>
+                </button>
+            </div>
+            <textarea class="input notepad-input"
+                [value]="tabs[activeTabIndex].content"
+                (input)="saveTabContent($event.target.value)"></textarea>
+            <div class="main-block-mask" *ngIf="showingTabRemoveModal">
+                <div class="container main-block-mask-content notepad-modal">
+                    <h4 class="notepad-modal-title">Are you really want to remove this tab?</h4>
+                    <div>
+                        <button class="btn" (click)="removeTab(tabToRemove)">Yes</button>
+                        <button class="btn" (click)="hideTabRemoveModal()">No</button>
+                    </div>
+                </div>
+            </div>
+            <div class="main-block-mask" *ngIf="showingTabCreateModal">
+                <div class="container main-block-mask-content notepad-modal">
+                    <h4 class="notepad-modal-title">Create new tab</h4>
+                    <form (submit)="createTab($event)">
+                        <input type="text" class="input" name="title" placeholder="Title">
+                        <button class="btn">Create</button>
+                    </form>
+                    <button class="btn-icon notepad-modal-hide-btn" (click)="hideTabCreateModal()" title="Hide">
                         <svg viewBox="0 0 24 24">
                             <use href="#cross"></use>
                         </svg>
                     </button>
-                    <input class="input notepad-title-input" type="text" #input
-                        (keyup.enter)="blurTitleInput(input)"
-                        (blur)="setTitle(input)"
-                        *ngIf="tab.settingTitle">
-                </li>
-                <li *ngIf="tabs.length > 4">
-                    <button class="btn-icon notepad-shift-btn" (click)="nextVisibleTabs()" [disabled]="shift + 4 >= tabs.length">
-                        <svg viewBox="0 0 24 24">
-                            <use href="#chevron-right"></use>
-                        </svg>
-                    </button>
-                </li>
-                <li>
-                    <button class="btn-icon notepad-create-new-btn" (click)="createNewTab()" title="Add new tab">
-                        <svg viewBox="0 0 24 24">
-                            <use href="#plus"></use>
-                        </svg>
-                    </button>
-                </li>
-            </ul>
-            <textarea class="input notepad-input" [value]="activeTabContent" (input)="saveTabContent($event.target.value)"></textarea>
-            <div class="main-block-mask" *ngIf="showingModal">
-                <div class="container main-block-mask-content notepad-modal">
-                    <p>Are you really want to remove this tab?</p>
-                    <div>
-                        <button class="btn" (click)="removeTab(tabToRemove)">Yes</button>
-                        <button class="btn" (click)="hideModal()">No</button>
-                    </div>
                 </div>
             </div>
         </div>
@@ -64,20 +80,16 @@ import { ChromeStorageService } from "../../services/chromeStorageService";
 export class Notepad {
     @Input() isVisible: boolean = false;
 
-    showingModal: boolean = false;
+    initialized: boolean = false;
+    showingTabRemoveModal: boolean = false;
+    showingTabCreateModal: boolean = false;
     shift: number = 0;
     activeTabIndex: number = 0;
     tabToRemove: number = 0;
     inputTimeoutId: number = 0;
-    activeTabContent: string = "";
-    tabs: Array<any>;
-    visibleTabs: Array<any> = [];
+    tabs: Array<any> = [];
 
-    constructor(
-        @Inject(DOCUMENT) private document,
-        private chromeStorageService: ChromeStorageService
-    ) {
-        this.document = document;
+    constructor(private chromeStorageService: ChromeStorageService) {
         this.chromeStorageService = chromeStorageService;
         this.tabs = [{
             title: "Tab",
@@ -89,127 +101,88 @@ export class Notepad {
         this.chromeStorageService.subscribeToChanges(this.chromeStorageChangeHandler.bind(this));
         this.chromeStorageService.get("notepad", storage => {
             this.tabs = storage.notepad || this.tabs;
-            this.setVisibleTabs();
             this.selectTab();
-        });
-    }
 
-    setVisibleTabs(shift = 0) {
-        this.visibleTabs = this.tabs.slice(shift, shift + 4);
+            this.initialized = true;
+        });
     }
 
     prevVisibleTabs() {
-        const index = this.activeTabIndex;
-
         this.shift -= 1;
-        this.setVisibleTabs(this.shift);
-        this.selectTab(index === this.visibleTabs.length - 1 ? index : index + 1);
+
+        if (this.activeTabIndex >= this.shift + 4) {
+            this.selectTab(this.activeTabIndex - 1);
+        }
     }
 
     nextVisibleTabs() {
-        const index = this.activeTabIndex;
-
         this.shift += 1;
-        this.setVisibleTabs(this.shift);
-        this.selectTab(index === 0 ? index : index - 1);
-    }
 
-    hideTitleInput() {
-        this.visibleTabs = this.visibleTabs.map(tab => {
-            tab.settingTitle = false;
-            return tab;
-        });
-    }
-
-    hideInputAndSelectTab(index) {
-        this.hideTitleInput();
-        this.selectTab(index);
+        if (this.activeTabIndex < this.shift) {
+            this.selectTab(this.activeTabIndex + 1);
+        }
     }
 
     selectTab(index = 0) {
         this.activeTabIndex = index;
-        this.activeTabContent = this.visibleTabs[index].content;
     }
 
-    createNewTab() {
-        this.hideTitleInput();
+    showTabCreateModal() {
+        this.showingTabCreateModal = true;
+    }
+
+    hideTabCreateModal() {
+        this.showingTabCreateModal = false;
+    }
+
+    createTab(event) {
+        event.preventDefault();
+
         this.tabs.push({
-            title: "Tab",
-            content: "",
-            settingTitle: true
+            title : event.target.elements.title.value.trim() || "Tab",
+            content: ""
         });
         this.shift = this.tabs.length - 4;
 
         if (this.shift < 0) {
             this.shift = 0;
         }
-        this.setVisibleTabs(this.shift);
-        this.selectTab(this.visibleTabs.length - 1);
-        setTimeout(() => {
-            this.document.querySelector(".notepad-title-input").focus();
-        });
+        this.selectTab(this.tabs.length - 1);
+        this.saveTabs();
+        this.hideTabCreateModal();
     }
 
     setTabForRemoval(index) {
-        if (this.tabs[this.shift + index].content) {
-            this.showingModal = true;
+        if (this.tabs[index].content) {
+            this.showingTabRemoveModal = true;
             this.tabToRemove = index;
+            this.selectTab(index);
         }
         else {
             this.removeTab(index);
         }
     }
 
-    hideModal() {
-        this.showingModal = false;
+    hideTabRemoveModal() {
+        this.showingTabRemoveModal = false;
     }
 
     removeTab(index) {
-        const tabIndex = this.shift + index;
+        this.activeTabIndex = 0;
+        this.shift = 0;
 
-        if (index === this.activeTabIndex) {
-            this.activeTabIndex = 0;
-            this.shift = 0;
-        }
-        else if (index < this.activeTabIndex) {
-            if (this.tabs.length < 5) {
-                this.activeTabIndex -= 1;
-            }
-            else if (this.shift > 0) {
-                this.shift -= 1;
-            }
-        }
-        else if (this.tabs.length > 4 && this.shift + 4 >= this.tabs.length) {
-            this.shift -= 1;
-            this.activeTabIndex += 1;
-        }
-        this.tabs.splice(tabIndex, 1);
-        this.setVisibleTabs(this.shift);
+        this.tabs.splice(index, 1);
         this.selectTab(this.activeTabIndex);
         this.saveTabs();
-        this.hideModal();
-    }
-
-    blurTitleInput(input) {
-        input.blur();
-        this.document.querySelector(".notepad-input").focus();
-    }
-
-    setTitle(input) {
-        const title = input.value.trim();
-        const tab = this.tabs[this.tabs.length - 1];
-
-        tab.title = title ? title : "Tab";
-        tab.settingTitle = false;
-        this.setVisibleTabs(this.shift);
-        this.saveTabs();
+        this.hideTabRemoveModal();
     }
 
     saveTabContent(content) {
+        this.tabs[this.activeTabIndex].content = content;
+
         clearTimeout(this.inputTimeoutId);
 
         this.inputTimeoutId = window.setTimeout(() => {
-            this.tabs[this.shift + this.activeTabIndex].content = content;
             this.saveTabs();
         }, 400);
     }
@@ -221,7 +194,6 @@ export class Notepad {
     chromeStorageChangeHandler({ notepad }) {
         if (notepad) {
             this.tabs = [...notepad.newValue];
-            this.setVisibleTabs();
             this.selectTab();
         }
     }
