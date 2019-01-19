3e9b8524007a083b4261ea3bb19771d6f629ac78
Andrius Kvedaras <ikandrius@gmail.com>
2019-01-19 17:34:26 +0200

    Refactor dropbox component

--- src/app/components/dropbox/dropbox.ts
+++ src/app/components/dropbox/dropbox.ts
@@ -1,4 +1,5 @@
 import { Component, Output, EventEmitter } from "@angular/core";
+import { DropboxService } from "../../services/dropboxService";
 import { SettingService } from "../../services/settingService";
 
 @Component({
@@ -8,179 +9,48 @@ import { SettingService } from "../../services/settingService";
 export class Dropbox {
     @Output() sessionEnded = new EventEmitter();
 
-    dropbox: any;
-    activeDir: any = null;
-    rootDir: object = {
-        name: "Root",
-        isDir: true,
-        path: "",
-        pathForDisplay: "/",
-        items: []
-    };
-    dropboxContents: any = JSON.parse(localStorage.getItem("dropbox")) || this.rootDir;
+    activeFolder: any = null;
+    dropboxFolder: any = null;
 
-    constructor(private settingService: SettingService) {}
+    constructor(
+        private dropboxService: DropboxService,
+        private settingService: SettingService) {}
 
     async ngOnInit() {
-        const { Dropbox }: any = await import("dropbox");
-        const token = localStorage.getItem("dropbox token");
-        this.dropbox = new Dropbox({ clientId: process.env.DROPBOX_API_KEY, fetch });
+        this.dropboxFolder = this.dropboxService.getDropbox();
 
-        if (token) {
-            this.init(token);
-        }
-        else {
-            this.login();
-        }
-    }
+        try {
+            await this.dropboxService.init();
 
-    init(token) {
-        this.dropbox.setAccessToken(token);
-
-        if (this.dropboxContents.cached) {
-            this.activeDir = this.dropboxContents;
-        }
-        else {
-            this.fetchDir(this.dropboxContents);
-        }
-    }
-
-    login() {
-        const authUrl = this.dropbox.getAuthenticationUrl(`${window.location.origin}/receiver.html`);
-        const authWindow: any = window.open(authUrl, "_blank", "width=640,height=480");
-        const intervalId = setInterval(() => {
-            if (authWindow.closed) {
-                clearInterval(intervalId);
-
-                if (authWindow.token) {
-                    this.init(authWindow.token);
-                    localStorage.setItem("dropbox token", authWindow.token);
-                }
-                else {
-                    this.sessionEnded.emit();
-                }
+            if (!this.dropboxFolder.cached) {
+                await this.dropboxService.fetchFolderItems(this.dropboxFolder);
+                this.saveDropbox();
             }
-        }, 800);
+            this.activeFolder = this.dropboxFolder;
+        } catch (e) {
+            console.log(e);
+            this.sessionEnded.emit();
+        }
     }
 
     logout() {
-        this.dropbox.authTokenRevoke();
-        this.dropbox.setAccessToken("");
-        localStorage.removeItem("dropbox");
-        localStorage.removeItem("dropbox token");
-
+        this.dropboxService.resetDropbox();
         this.sessionEnded.emit();
-
-        this.deleteServiceWorkerCache();
     }
 
-    deleteServiceWorkerCache() {
-        caches.keys().then(keys => {
-            keys.forEach(key => {
-                if (key.startsWith("dropbox")) {
-                    caches.delete(key);
-                }
-            });
-        });
-    }
-
-    getThumbnail(path) {
-        return new Promise(resolve => {
-            const headers = new Headers({
-                "Authorization": `Bearer ${this.dropbox.accessToken}`,
-                "Dropbox-API-Arg": JSON.stringify({ path, size: "w32h32" })
-            });
-
-            fetch("https://content.dropboxapi.com/2/files/get_thumbnail", {
-                method: "GET",
-                headers
-            })
-            .then(response => response.blob())
-            .then(blob => {
-                const image = new Image();
-
-                image.onload = () => {
-                    const canvas = document.createElement("canvas");
-                    const ctx = canvas.getContext("2d");
-
-                    canvas.width = image.width;
-                    canvas.height = 20;
-                    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-                    resolve(canvas.toDataURL("image/jpeg", 0.4));
-                };
-                image.src = window.URL.createObjectURL(blob);
-            });
-        });
-    }
-
-    getDir(items, dirNames) {
-        const dir = items.find(item => item.name.toLowerCase() === dirNames[0]);
-
-        if (dirNames.length === 1) {
-            return dir;
+    getFolder(folder, name) {
+        if (folder.items.find(item => item.name === name)) {
+            return folder;
         }
-        dirNames.shift();
-        return this.getDir(dir.items, dirNames);
+        for (const item of folder.items) {
+            if (this.getFolder(item, name)) {
+                return item;
+            }
+        }
     }
 
     goBack() {
-        const dirNames = this.activeDir.path.split("/").slice(1);
-
-        if (dirNames.length === 1) {
-            this.activeDir = this.dropboxContents;
-        }
-        else {
-            dirNames.pop();
-            this.activeDir = this.getDir(this.dropboxContents.items, dirNames);
-        }
-    }
-
-    fetchDir(dir) {
-        dir.fetching = true;
-
-        return this.dropbox.filesListFolder({ path: dir.path }).then(res => {
-            res.entries.forEach(entry => {
-                const newEntry: any = {
-                    name: entry.name,
-                    isDir: entry[".tag"] === "folder",
-                    path: entry.path_lower,
-                    pathForDisplay: entry.path_display,
-                    fetching: false
-                };
-
-                if (newEntry.isDir) {
-                    newEntry.cached = false;
-                    newEntry.items = [];
-                    newEntry.icon = "folder";
-                }
-                else {
-                    newEntry.icon = "file";
-
-                    if (this.isImage(entry.name)) {
-                        this.getThumbnail(newEntry.path).then(thumbnail => {
-                            newEntry.thumbnail = thumbnail;
-                            this.saveDropbox();
-                        });
-                    }
-                }
-                dir.items.push(newEntry);
-            });
-            this.activeDir = dir;
-            dir.cached = true;
-            dir.fetching = false;
-            this.saveDropbox();
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    }
-
-    isImage(name) {
-        return /(\.jpe?g|\.png|\.gif|\.bmp)$/i.test(name);
-    }
-
-    makeImageDisplayable(image) {
-        return image.url.replace(/0$/, "1");
+        this.activeFolder = this.getFolder(this.dropboxFolder, this.activeFolder.name);
     }
 
     setBackground(url) {
@@ -189,62 +59,42 @@ export class Dropbox {
         });
     }
 
-    setImageAsBackground(item) {
+    async setImageAsBackground(item) {
         if (item.url) {
             this.setBackground(item.url);
             return;
         }
-        item.fetching = true;
-
-        this.dropbox.sharingGetSharedLinks({ path: item.path }).then(res => {
-            const image = res.links.find(link => link.path.includes(item.name));
-
-            if (image) {
-                return this.makeImageDisplayable(image);
-            }
-            return this.dropbox.sharingCreateSharedLinkWithSettings({ path: item.path })
-                .then(this.makeImageDisplayable);
-        })
-        .then(url => {
-            item.url = url;
-            item.fetching = false;
-
-            this.setBackground(url);
-            this.saveDropbox();
-        })
-        .catch(error => {
-            console.log(error);
-            item.fetching = false;
-        });
+        item.url = await this.dropboxService.fetchImageUrl(item);
+        this.setBackground(item.url);
     }
 
-    selectItem(item) {
-        if (!item.isDir) {
-            if (this.isImage(item.name) && !item.fetching) {
-                this.setImageAsBackground(item);
-            }
-            else {
-                item.error = true;
-                item.fetching = false;
-
-                setTimeout(() => {
-                    item.error = false;
-                }, 800);
-            }
+    async selectItem(item) {
+        if (item.fetching) {
             return;
         }
+        item.fetching = true;
 
-        if (item.cached) {
-            this.activeDir = item;
-            return;
+        if (item.isFolder) {
+            if (!item.cached) {
+                await this.dropboxService.fetchFolderItems(item);
+            }
+            this.activeFolder = item;
         }
+        else if (item.isImage) {
+            await this.setImageAsBackground(item);
+        }
+        else {
+            item.error = true;
 
-        if (!item.fetching) {
-            this.fetchDir(item);
+            setTimeout(() => {
+                delete item.error;
+            }, 800);
         }
+        delete item.fetching;
+        this.saveDropbox();
     }
 
     saveDropbox() {
-        localStorage.setItem("dropbox", JSON.stringify(this.dropboxContents));
+        this.dropboxService.saveDropbox(this.dropboxFolder);
     }
 }
