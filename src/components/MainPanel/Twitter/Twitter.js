c11f4864d149283861772ff615bb7c64a196ec9e
Andrius Kvedaras <ikandrius@gmail.com>
2022-03-11 17:01:48 +0200

    Add ability to change tweet highlight color

--- src/components/MainPanel/Twitter/Twitter.js
+++ src/components/MainPanel/Twitter/Twitter.js
@@ -6,6 +6,7 @@ import Dropdown from "components/Dropdown";
 import Spinner from "components/Spinner";
 import "./twitter.css";
 import Form from "./Form";
+import Settings from "./Settings";
 import UserCard from "./UserCard";
 import Tweet from "./Tweet";
 
@@ -20,6 +21,8 @@ export default function Twitter({ showIndicator }) {
   const [fetchingMoreTweets, setFetchingMoreTweets] = useState(false);
   const [changingUser, setChangingUser] = useState(false);
   const [userCard, setUserCard] = useState({ reveal: false });
+  const [hightlightColor, setHightlightColor] = useState(() => localStorage.getItem("twitter-highlight-color"));
+  const [settingsOpened, setSettingsOpened] = useState(false);
   const tweetsRef = useRef(null);
   const lastUpdate = useRef(0);
   const timelineTimeoutId = useRef(0);
@@ -59,6 +62,10 @@ export default function Twitter({ showIndicator }) {
         setTweets(timeline.tweets);
         setUsers([...addUser(user)]);
         setActiveUser({ ...getActiveUser() });
+
+        if (!hightlightColor) {
+          setHightlightColor(user.profileColor);
+        }
 
         if (addingAnotherUser) {
           setAddingAnotherUser(false);
@@ -236,20 +243,35 @@ export default function Twitter({ showIndicator }) {
     setTweets([...tweets]);
   }
 
+  function updateHighlightColor(color) {
+    setHightlightColor(color);
+  }
+
+  function openSettings() {
+    setSettingsOpened(true);
+  }
+
+  function closeSettings() {
+    setSettingsOpened(false);
+  }
+
   if (loading || !user || addingAnotherUser) {
     return <Form initialLoading={loading} loadContent={loadContent} addingAnotherUser={addingAnotherUser} hide={hideAnotherUserForm}/>;
   }
   return (
-    <div className="twitter-content" style={{ "--profile-color": user.profileColor }}>
-      <div className="main-panel-item-header">
+    <div className="twitter-content" style={{ "--highlight-color": hightlightColor }}>
+      <div className="main-panel-item-header twitter-header">
         <a href={user.homepage} className="twitter-user" target="_blank" rel="noreferrer">
           <img src={user.profileImage} className="twitter-user-image" alt=""/>
           <span className="twitter-user-name">{user.name}</span>
           <span className="twitter-user-handle">{user.handle}</span>
         </a>
+        <button className="btn icon-btn twitter-header-btn" onClick={openSettings} title="Settings">
+          <Icon id="settings"/>
+        </button>
         {users && (
           <Dropdown
-            toggle={{ className: "main-panel-item-header-btn" }}
+            container={{ className: "twitter-header-btn" }}
             body={{ className: "twitter-header-dropdown" }}>
             <div className="dropdown-group">
               {users.map((user, i) => (
@@ -296,6 +318,11 @@ export default function Twitter({ showIndicator }) {
         )}
       </ul>
       <ToTop/>
+      {settingsOpened && (
+        <div className="twitter-content-mask">
+          <Settings defaultColor={user.profileColor} updateHighlightColor={updateHighlightColor} close={closeSettings}/>
+        </div>
+      )}
       {changingUser && (
         <div className="twitter-content-mask">
           <Spinner/>
