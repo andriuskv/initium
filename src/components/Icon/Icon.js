1a78423dd450faa91f0a8dc1c5c68a836cf86a9a
Andrius Kvedaras <ikandrius@gmail.com>
2023-08-05 14:31:47 +0300

    Remove redundant files

--- src/components/Icon/Icon.js
+++
@@ -1,8 +0,0 @@
-export default function Icon({ id, className, style, title }) {
-  return (
-    <svg viewBox="0 0 24 24" className={className} style={style}>
-      {title && <title>{title}</title>}
-      <use href={`#${id}`}></use>
-    </svg>
-  );
-}
