c5f67502918fb815bc2cde7f5ec2d44804f8293f
Andrius Kvedaras <ikandrius@gmail.com>
2023-09-10 15:58:48 +0300

    Add ability to manually change sticky note color

--- src/components/StickyNotes/Form/Form.jsx
+++ src/components/StickyNotes/Form/Form.jsx
@@ -1,5 +1,16 @@
 import { useState, useEffect, useLayoutEffect, useRef } from "react";
+import Icon from "components/Icon";
+import Dropdown from "components/Dropdown";
 import "./form.css";
+
+const colors = [
+  "#0d99ff", "#14b88f", "#8fe935", "#ffcd29",
+  "#359ee9", "#14ae5c", "#66ca02", "#fcf403",
+  "#6cb2e5", "#68cab1", "#ade07b", "#e3e078",
+  "#ffa629", "#f24822", "#e935da", "#a34bfb",
+  "#e9bc35", "#d24b2d", "#c20ab3", "#8f35e9",
+  "#e3bb82", "#e0755c", "#e278da", "#ad7edd"
+];
 
 export default function Form({ initialForm, noteCount, createNote, discardNote, showForm }) {
   const [movable, setMovable] = useState(false);
@@ -13,8 +24,6 @@ export default function Form({ initialForm, noteCount, createNote, discardNote, 
     }
 
     if (initialForm.action === "create") {
-      const colors = ["#0D99FF", "#9747FF", "#FF24BD", "#F24822", "#FFA629", "#FFCD29", "#14AE5C", "#00A1C2"];
-
       setForm({
         action: initialForm.action,
         id: crypto.randomUUID(),
@@ -91,6 +100,10 @@ export default function Form({ initialForm, noteCount, createNote, discardNote, 
     setForm({ ...form, [event.target.name]: event.target.value });
   }
 
+  function updateColor(color) {
+    setForm({ ...form, color });
+  }
+
   function saveNote() {
     createNote(form);
     discardNote();
@@ -108,7 +121,7 @@ export default function Form({ initialForm, noteCount, createNote, discardNote, 
     return null;
   }
   return (
-    <div className={`sticky-note sticky-note-form${movable ? " movable" : ""} ${editable ? " editable" : ""}`} key={form.id}
+    <div className={`sticky-note sticky-note-form${movable ? " movable" : ""}${editable ? " editable" : ""}`} key={form.id}
       style={{ "--x": form.x, "--y": form.y, "--tilt": form.tilt, "--background-color": form.color }}>
       <div className="sticky-note-drag-handle" onPointerDown={enableNoteDrag} title={movable ? "" : "Move"}></div>
       <input className="input sticky-note-title sticky-note-input" name="title" onChange={handleInputChange}
@@ -116,10 +129,24 @@ export default function Form({ initialForm, noteCount, createNote, discardNote, 
       <textarea className="input sticky-note-input sticky-note-content-input" name="content" onChange={handleInputChange}
         value={form.content} placeholder={`Content #${form.action === "edit" ? form.index + 1 : noteCount + 1}`}></textarea>
       {editable ? (
-        <div className="sticky-note-btns">
-          <button className="btn" onClick={discardNote}>Discard</button>
-          <button className="btn" onClick={saveNote}>Save</button>
-        </div>
+        <>
+          <div className="sticky-note-sidebar">
+            <Dropdown toggle={{ iconId: "color-picker", title: "Color picker" }} body={{ className: "sticky-note-dropdown" }}>
+              <ul className="sticky-note-color-picker-items">
+                {colors.map(color => (
+                  <li key={color}>
+                    <button className={`btn sticky-note-color-picker-item${form.color === color ? " active" : ""}`}
+                      onClick={() => updateColor(color)} style={{ backgroundColor: color }}></button>
+                  </li>
+                ))}
+              </ul>
+            </Dropdown>
+          </div>
+          <div className="sticky-note-btns">
+            <button className="btn" onClick={discardNote}>Discard</button>
+            <button className="btn" onClick={saveNote}>Save</button>
+          </div>
+        </>
       ) : null}
     </div>
   );
